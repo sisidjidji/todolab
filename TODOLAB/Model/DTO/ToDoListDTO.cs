12fed72aae6fcaf199e3b30f74ddd73d47388256
sihem azibi <s.azibi@live.fr>
2020-06-08 22:26:18 -0500

    created DTO for todolist

---
+++ TODOLAB/Model/DTO/ToDoListDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODOLAB.Model.DTO
+{
+    public class ToDoListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime? DueDate { get; set; }
+    }
+}
