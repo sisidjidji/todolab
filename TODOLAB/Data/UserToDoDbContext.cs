1a775a47ef82bdde9be6907dfe4ad494ce5f3b69
sihem azibi <s.azibi@live.fr>
2020-06-08 21:34:11 -0500

    user model done

---
+++ TODOLAB/Data/UserToDoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODOLAB.Data
+{
+    public class UserToDoDbContext :DbContext
+    {
+        public UserToDoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
