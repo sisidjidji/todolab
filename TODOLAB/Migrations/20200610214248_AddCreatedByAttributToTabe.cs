d5ed1ff126bbad9c845c90655bb96542b3cc7245
sihem azibi <s.azibi@live.fr>
2020-06-10 16:43:35 -0500

    added migration to todoDbContext and updated database

---
+++ TODOLAB/Migrations/20200610214248_AddCreatedByAttributToTabe.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TODOLAB.Migrations
+{
+    public partial class AddCreatedByAttributToTabe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserId",
+                table: "ToDoList",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "ToDoList");
+        }
+    }
+}
