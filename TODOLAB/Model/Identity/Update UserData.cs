b71d2feea5dad009293032360de64794d9e68a81
sihem azibi <s.azibi@live.fr>
2020-06-09 20:38:31 -0500

    updateUserData model done

---
+++ TODOLAB/Model/Identity/Update UserData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TODOLAB.Model.Identity
+{
+    public class Update_UserData
+    {
+        [Required]
+
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+    }
+}
