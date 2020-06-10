caeeb5f7964dac9bdf135be258eb10f4bd1177ce
sihem azibi <s.azibi@live.fr>
2020-06-09 20:40:06 -0500

    userWithToken model created

---
+++ TODOLAB/Model/Identity/UserWithToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODOLAB.Model.Identity
+{
+    public class UserWithToken
+    {
+        public string UserId { get; set; }
+        public String Token { get; set; }
+    }
+}
