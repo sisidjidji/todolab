056ff10202c61cf2065acb01e8bf6271267565ba
sihem azibi <s.azibi@live.fr>
2020-06-09 21:18:31 -0500

    login Task created

--- TODOLAB/Controllers/UserToDoController.cs
+++ TODOLAB/Controllers/UserToDoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TODOLAB.Model.Identity;
 using TODOLAB.Repositories;
 
 namespace TODOLAB.Controllers
@@ -18,5 +19,25 @@ namespace TODOLAB.Controllers
             this.userManager = userManager;
         }
 
+        [HttpPost("Login")]
+        public async Task <IActionResult> Login(LoginData login)
+        {
+            var user = await userManager.FindByNameAsync(login.Username);
+            if(user!=null)
+            {
+                var result = await userManager.CheckPasswordAsync(user, login.Password);
+                if (result)
+                {
+                    return Ok(new UserWithToken
+                    { 
+                        UserId=user.Id,
+                        Token=userManager.CreateToken(user)
+
+                    });
+                }
+
+                await userManager.AccessFailedAsync(user);
+        }
+            return Unauthorized ();
     }
 }
